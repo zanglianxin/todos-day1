4b758281476255a5815181720aa7a8da8034647f
fengbujue <1125238900@qq.com>
2022-07-28 20:32:39 +0800

    todos初始化

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <button>换肤</button>
+    <div class="todoapp">
+      <todo-header></todo-header>
+      <todo-main></todo-main>
+      <todo-footer></todo-footer>
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoFooter from './components/TodoFooter.vue' // shift + alt + 下
+import TodoHeader from './components/TodoHeader.vue' // shift + alt + 下
+import TodoMain from './components/TodoMain.vue' // shift + alt + 下
+export default {
+  data() {
+    return {}
+  },
+
+  components: {
+    TodoFooter,
+    TodoHeader,
+    TodoMain,
+  },
+
+  created() {},
+
+  methods: {},
+}
+</script>
+
+<style scoped></style>
---
+++ src/components/TodoMain.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul class="todo-list">
+    <!-- completed: 完成的类名 -->
+    <li class="completed" >
+      <div class="view">
+        <input class="toggle" type="checkbox" />
+        <label>任务名</label>
+        <button class="destroy"></button>
+      </div>
+    </li>
+  </ul>
+  
+</template>
+
+<script>
+export default {
+}
+</script>
